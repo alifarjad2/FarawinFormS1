2e5d15277ec3789a1f88cd6c415fc2899d5561be
Hame:D <hamedsmart20@gmail.com>
2023-07-05 13:21:54 +0330

    ایجاد مخاطب فرضی و چک کردن ان روی دکمه افزودن

--- hamedAbdollahzade/js_addContact.js
+++ hamedAbdollahzade/js_addContact.js
@@ -1,12 +1,14 @@
+    const contacts = [{name:"hamedAbdollahzade", mobile:'09333536546'}]
+// -------------------------------------------------------------------------------------------------------------------------------------------
 // اینجا ی فانکشن تعریف کردم برای عملیات اعتبار سنجی فیلد های شماره موبایل و نام کاربر ک اگه درست نبود دکمه افزودن غیرفعال بشه
 // و در کنار المنت اینپوت کاربر بیاد پیغام خطا رو بهش نشون بده 
 function validation ()
 {
-    // debugger
-    //    اینجا دکمه و مقادیر موبایل و نام و نام خانوادگی رو گرفتم و از نوع ور تعریف کردم ک پایین تر بتونم ازش استفاده کنم
-    var btnAddContact = document.getElementById('btnAddContact') ;
-    var flname = document.getElementById('inputFnameLname').value;
-    var phoneContact = document.getElementById('inputMobile').value ;
+    //  debugger
+   
+     btnAddContact = document.getElementById('btnAddContact') ;
+     flname = document.getElementById('inputFnameLname').value;
+     phoneContact = document.getElementById('inputMobile').value ;
     
 // اینجا مقدار شماره تماس مخاطب رو میگیره و چک میکنه ک حتما رقم باشه و طولش یازده رقم باشه 
     if (Number(phoneContact) && phoneContact.length === 11 )
@@ -46,3 +48,16 @@ function validation ()
     }
 }
 // ------------------------------------------------------------------
+  function addContact() 
+  {
+    if (flname === contacts[0].name && phoneContact === contacts[0].mobile )
+    {
+        document.getElementById('spanInputButton').style.display="none";
+        alert ('امکان گفتگو با این مخاطب وجود دارد');
+    }
+    else
+    {
+        document.getElementById('spanInputButton').style.display="";
+        alert('این مخاطب وجود ندارد')
+    }
+  }
